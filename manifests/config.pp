85815cea714df9fbaa71bd72a353e949aea08516
Jason Clark <jason@clarkhome.us>
2012-05-12 15:49:06 -0500

    files needed for centos

--- manifests/config.pp
+++ manifests/config.pp
@@ -55,6 +55,36 @@ class php::config {
           ensure  => file,
           content => $php::php_ini_content,
           source  => $php::php_ini_source;
+        "${php::params::conf_dir}curl.ini":
+          owner   => root,
+          group   => root,
+          require => Class["php::install"],
+          ensure  => file,
+          source  => "puppet://modules/php/modules_ini/curl.ini";
+        "${php::params::conf_dir}fileinfo.ini":
+          owner   => root,
+          group   => root,
+          require => Class["php::install"],
+          ensure  => file,
+          source  => "puppet://modules/php/modules_ini/fileinfo.ini";
+        "${php::params::conf_dir}json.ini":
+          owner   => root,
+          group   => root,
+          require => Class["php::install"],
+          ensure  => file,
+          source  => "puppet://modules/php/modules_ini/json.ini";
+        "${php::params::conf_dir}phar.ini":
+          owner   => root,
+          group   => root,
+          require => Class["php::install"],
+          ensure  => file,
+          source  => "puppet://modules/php/modules_ini/phar.ini";
+        "${php::params::conf_dir}zip.ini":
+          owner   => root,
+          group   => root,
+          require => Class["php::install"],
+          ensure  => file,
+          source  => "puppet://modules/php/modules_ini/zip.ini";
       }
     }
   }
